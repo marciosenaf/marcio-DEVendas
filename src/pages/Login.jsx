57d4208189fe1e8297ebda7d567d95472ade253f
Márcio Sena Santos Filho <97891534+marciosenaf@users.noreply.github.com>
2022-10-20 00:13:48 -0300

    added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React { useState }from "react";
+import Helmet from '../components/Helmet/Helmet'
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import '../styles/login.css'
 
 const Login = () => {
-    return <div>Login</div>
+const [email, setEmail]
+    return <Helmet title='Login'>
+    <section>
+        <Container>
+            <Row>
+                <Col lg='6' className="m-auto text-center" >
+                <h3 className="fw-bold fs-4">Login</h3>
+                
+                <Form className="auth__form">
+                    <FormGroup className="form__group">
+                        <input type="email" placeholder="Enter your email" />
+                    </FormGroup>
+                    <FormGroup className="form__group">
+                        <input type="password" placeholder="Enter your password" />
+                    </FormGroup>
+                <button className="buy__btn auth__btn">Login</button>
+                <p>Don't have an account? <Link to="/signup"> Create an account</Link></p>
+                </Form>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+    </Helmet>
 };
 
 export default Login;
