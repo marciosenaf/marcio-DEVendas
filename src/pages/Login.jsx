3df9236041b011370c5e311acaae439515327e8f
Márcio Sena Santos Filho <97891534+marciosenaf@users.noreply.github.com>
2022-10-20 06:10:29 -0300

    added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
-import React { useState }from "react";
+import React, { useState }from "react";
 import Helmet from '../components/Helmet/Helmet'
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import { Link } from "react-router-dom";
 import '../styles/login.css'
 
 const Login = () => {
-const [email, setEmail]
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
     return <Helmet title='Login'>
     <section>
         <Container>
@@ -15,10 +17,12 @@ const [email, setEmail]
                 
                 <Form className="auth__form">
                     <FormGroup className="form__group">
-                        <input type="email" placeholder="Enter your email" />
+                        <input type="email" placeholder="Enter your email"
+                        value={email} onChange={e=> setEmail(e.target.value) }/>
                     </FormGroup>
                     <FormGroup className="form__group">
-                        <input type="password" placeholder="Enter your password" />
+                        <input type="password" placeholder="Enter your password" 
+                        value={password} onChange={e=> setPassword(e.target.value) }/>
                     </FormGroup>
                 <button className="buy__btn auth__btn">Login</button>
                 <p>Don't have an account? <Link to="/signup"> Create an account</Link></p>
