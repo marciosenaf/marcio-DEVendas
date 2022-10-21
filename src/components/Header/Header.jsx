dd62d3e766641756b930431204787245ba2fa39c
Márcio Sena Santos Filho <97891534+marciosenaf@users.noreply.github.com>
2022-10-21 17:31:14 -0300

    added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
-import React, { useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import './header.css'
-import { NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from '../../assets/images/eco-logo.png'
 import { Container, Row } from "reactstrap";
 import userIcon from '../../assets/images/user-icon.png'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useSelector } from "react-redux";
+import useAuth from "../../custom-hooks/useAuth";
 const nav__link = [
     {
         path: 'home',
@@ -27,13 +28,14 @@ const Header = () => {
     const totalQuantity = useSelector(state => state.cart.totalQuantity)
     const menuRef = useRef(null)
     const navigate = useNavigate()
+    const { currentUser } = useAuth()
 
     const stickyHeaderFunc = () => {
-        window.addEventListener("scroll", ()=> {
+        window.addEventListener("scroll", () => {
             if (
                 document.body.scrollTop > 80 ||
-                document.documentElement.scrollTop > 80 
-                ) {
+                document.documentElement.scrollTop > 80
+            ) {
                 headerRef.current.classList.add("sticky__header")
             } else {
                 headerRef.current.classList.remove("sticky__header")
@@ -45,11 +47,11 @@ const Header = () => {
         stickyHeaderFunc()
 
         return () => window.removeEventListener("scroll", stickyHeaderFunc);
-    } )
+    })
 
     const menuToggle = () => menuRef.current.classList.toggle('active__menu')
 
-    const navigateToCart =()=>{
+    const navigateToCart = () => {
         navigate('/cart')
     }
 
@@ -66,17 +68,17 @@ const Header = () => {
 
                     <div className="navigation" ref={menuRef} onClick={menuToggle} >
                         <ul className="menu">
-                        {
+                            {
                                 nav__link.map((item, index) => (
                                     <li className="nav__item" key={index} >
-                                        <NavLink 
-                                        to={item.path} 
-                                        className={(navClass) => 
-                                        navClass.isActive ? 'nav__active' : ""} >
-                                        {item.display}</NavLink>
+                                        <NavLink
+                                            to={item.path}
+                                            className={(navClass) =>
+                                                navClass.isActive ? 'nav__active' : ""} >
+                                            {item.display}</NavLink>
                                     </li>
                                 ))}
-                                
+
                         </ul>
                     </div>
 
@@ -84,22 +86,35 @@ const Header = () => {
                         <span className="fav__icon">
                             <i class="ri-heart-line" ></i>
                             <span className="badge">1</span>
-                        </span>     
+                        </span>
                         <span className="cart__icon" onClick={navigateToCart} >
                             <i class="ri-shopping-bag-line"></i>
                             <span className="badge">{totalQuantity}</span>
                         </span>
+
+                        <div className="profile">
+                            <motion.img whileTap={{ scale: 1.2 }} src={
+                                currentUser ? currentUser.photoURL : userIcon
+                            } alt="" />
                         
-                        <span>
-                            <motion.img whileTap={{scale:1.2}} src={userIcon} alt="" />
-                        </span>
+                        <div className="profile__actions">
+                            {
+                                    currentUser ? <span>Logout</span> :
+                                    <div>
+                                        <Link to></Link>
+                                    </div>
+                            }
+                        </div>
+
+                        </div>
+
                         <div className="mobile__menu">
-                        <span onClick={menuToggle} >
-                            <i class="ri-menu-line" ></i>
-                        </span>
+                            <span onClick={menuToggle} >
+                                <i class="ri-menu-line" ></i>
+                            </span>
+                        </div>
                     </div>
-                    </div>
-                    
+
                 </div>
             </Row>
         </Container>
