f20679ff449fa4f2fe282cd73fca150595861d74
Márcio Sena Santos Filho <97891534+marciosenaf@users.noreply.github.com>
2022-10-21 16:47:36 -0300

    added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ try{
     console.log(user)
     setLoading(false)
     toast.success()
-    Navigate('/checkout')
+    navigate('/checkout')
 
 } catch(error) {
 
@@ -45,22 +45,36 @@ try{
     <section>
         <Container>
             <Row>
-                <Col lg='6' className="m-auto text-center" >
+                {
+                    loading? (
+                        <Col lg='12' className="text-center" >
+                        
+                        <h5 className="fw-bold">Loading...</h5></Col>
+                        
+                        ):( 
+                        
+                        <Col lg='6' className="m-auto text-center" >
                 <h3 className="fw-bold fs-4">Login</h3>
                 
                 <Form className="auth__form" onSubmit={signIn}>
+
                     <FormGroup className="form__group">
                         <input type="email" placeholder="Enter your email"
                         value={email} onChange={e=> setEmail(e.target.value) }/>
                     </FormGroup>
+
                     <FormGroup className="form__group">
                         <input type="password" placeholder="Enter your password" 
                         value={password} onChange={e=> setPassword(e.target.value) }/>
                     </FormGroup>
+
                 <button className="buy__btn auth__btn" type="submit">Login</button>
+
                 <p>Don't have an account? <Link to="/signup"> Create an account</Link></p>
                 </Form>
                 </Col>
+                    )}
+                   
             </Row>
         </Container>
     </section>
